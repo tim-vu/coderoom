2694306028e1a73fbb9377825923555b56113f5d
Tim vuegen <tim.vuegen@hotmail.com>
2020-08-20 01:16:54 +0200

    Update build script to install dev dependencies

--- Frontend/build.sh
+++ Frontend/build.sh
@@ -6,16 +6,13 @@ mkdir public/app
 npm install --prefix App
 npm run --prefix App build; exit_1=$?
 
-exit_1=0
-
 cp -r ./App/build/* ./public/app/
 
 export NODE_ENV=production
 
 npm install --prefix Landing
+npm install --prefix Landing --only=dev 
 npm run --prefix Landing build; exit_2=$?
-
-exit_2=1
 
 cp -r ./Landing/build/* ./public/
 
