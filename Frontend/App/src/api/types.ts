2fc29044ea51013d136049c7ba39b0660a2b383f
Tim vuegen <tim.vuegen@hotmail.com>
2020-08-24 22:36:28 +0200

    Add missing property to JavaScript Language

--- Frontend/App/src/api/types.ts
+++ Frontend/App/src/api/types.ts
@@ -32,7 +32,7 @@ export const Languages: Language[] = [
   { key: 0, name: "Java", aceMode: "java" },
   { key: 1, name: "C#", aceMode: "csharp" },
   { key: 2, name: "Python3", aceMode: "python" },
-  { key: 3, name: "JavaScript" },
+  { key: 3, name: "JavaScript", aceMode: "javascript" },
 ];
 
 export function getLanguageByKey(languageKey: number): Language | undefined {
